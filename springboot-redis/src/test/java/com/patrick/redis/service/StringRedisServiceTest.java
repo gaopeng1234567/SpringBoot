c80233cda41b74b1b6af19a4350de4239ec828f1
gpatrick <657881431@qq.com>
2021-03-28 16:12:07 +0800

    redis哨兵模式测试

--- springboot-redis/src/test/java/com/patrick/redis/service/StringRedisServiceTest.java
+++ springboot-redis/src/test/java/com/patrick/redis/service/StringRedisServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
@@ -14,6 +15,7 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("sentinel-redis")
 public class StringRedisServiceTest {
     @Autowired
     private StringRedisService redisService;
